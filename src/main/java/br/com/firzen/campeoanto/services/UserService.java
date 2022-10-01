b390add0a6f90fcc8ddc53b774419112381473ac
Augusto <augusto@imd.ufrn.br>
2022-10-01 00:31:21 -0300

    add para o heroku

---
+++ src/main/java/br/com/firzen/campeoanto/services/UserService.java
@@ -0,0 +1,10 @@
+package br.com.firzen.campeoanto.services;
+
+import org.springframework.stereotype.Service;
+
+import br.com.firzen.campeoanto.model.User;
+
+@Service
+public class UserService extends AbstractService<User>{
+
+}
